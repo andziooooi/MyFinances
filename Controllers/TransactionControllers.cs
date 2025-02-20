33480e0e7f665d40d14605cea214570bff1354fc
andziooooi <andrzyk@gmail.com>
2025-02-20 04:04:29 +0100

    Dodaj pliki projektów.

---
+++ Controllers/TransactionControllers.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFinances.Models;
+using System;
+
+namespace MyFinances.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public IActionResult Index(int? year, int? month)
+        {
+            int currentYear = year ?? DateTime.Now.Year;
+            int currentMonth = month ?? DateTime.Now.Month;
+
+            var model = new CalendarViewModel(currentYear, currentMonth);
+            return View(model);
+        }
+    }
+}
+
---
+++ Models/CalendarViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MyFinances.Models
+{
+    public class CalendarViewModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public List<DateTime> Days { get; set; } = new List<DateTime>();
+
+        public CalendarViewModel(int year, int month)
+        {
+            Year = year;
+            Month = month;
+
+            GenerateCalendar();
+        }
+
+        private void GenerateCalendar()
+        {
+            Days.Clear();
+            DateTime firstDay = new DateTime(Year, Month, 1); 
+            
+            int daysInMonth = DateTime.DaysInMonth(Year, Month); 
+
+            int firstDayOfWeek = (int)firstDay.DayOfWeek -1; 
+
+            // Znalezienie pierwszego dnia w siatce kalendarza
+            DateTime startDate = firstDay.AddDays(-firstDayOfWeek);
+
+            // Tworzenie siatki kalendarza (6 tygodni x 7 dni = 42)
+            for (int i = 0; i < 42; i++)
+            {
+                Days.Add(startDate.AddDays(i));
+            }
+        }
+    }
+}
