3fe0f9e9d94af371ee6846259c0fba1cfdc01565
andziooooi <andrzyk@gmail.com>
2025-02-23 01:50:29 +0100

    Prepared database models

---
+++ Models/Transactions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFinances.Models
+{
+    public class Transactions
+    {
+        [Key]
+        public int ID { get; set; }
+        public double Amount { get; set; }
+        public int Type { get; set; }
+        public DateTime Date { get; set; }
+        public int UserID { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
---
+++ Models/UserCategories.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFinances.Models
+{
+    public class UserCategories
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int UserID { get; set; }
+        public virtual List<Transactions> Transactions { get; set; }
+    }
+}
